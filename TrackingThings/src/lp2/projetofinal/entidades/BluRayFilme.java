1293173a438a6d17146ba29d326e957ac354df9e
marcelovitorino <marcelo.vitorino@ccc.ufcg.edu.br>
2017-08-01 21:43:12 -0300

    .

--- TrackingThings/src/lp2/projetofinal/entidades/BluRayFilme.java
+++ TrackingThings/src/lp2/projetofinal/entidades/BluRayFilme.java
@@ -1,7 +1,11 @@
 package lp2.projetofinal.entidades;
 
+import lp2.projetofinal.enums.GeneroBluRay;
+import lp2.projetofinal.enums.GeneroBluRay;;
+
 public class BluRayFilme extends BluRay {
 	private int anoLancamento;
+	private GeneroBluRay genero = GeneroBluRay.OUTRO;
 	
 	public BluRayFilme(String nome, double preco,int duracao, String classificacao,int novoAnoLancamento) {
 		super(nome,preco,duracao,classificacao);
