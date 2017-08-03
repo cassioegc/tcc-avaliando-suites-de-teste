0302cba1d3ea7550dafc78ec7e25ab828891ec88
Thiago Santos <thiago.moura@ccc.ufcg.edu.br>
2017-08-03 10:28:18 -0300

    Episodio criado

---
+++ TrackingThings/src/lp2/projetofinal/entidades/EpisodioSerie.java
@@ -0,0 +1,14 @@
+package lp2.projetofinal.entidades;
+
+public class EpisodioSerie {
+	
+	private int duracao;
+	
+	public EpisodioSerie(int duracao){
+		this.duracao = duracao;
+	}
+
+	public int getDuracao() {
+		return duracao;
+	}
+}
