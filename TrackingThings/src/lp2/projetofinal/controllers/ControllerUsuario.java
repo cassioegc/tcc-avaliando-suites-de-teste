26a2afe320b8fbc279c4e7254d934871650a2033
Thiago Santos <thiago.moura@ccc.ufcg.edu.br>
2017-08-10 19:20:55 -0300

    Controller Usuario e Emprestimo documentado!

--- TrackingThings/src/lp2/projetofinal/controllers/ControllerUsuario.java
+++ TrackingThings/src/lp2/projetofinal/controllers/ControllerUsuario.java
@@ -26,6 +26,17 @@ public class ControllerUsuario {
 		usuarios = new HashMap<ChaveNomeTelefone, Usuario>();
 	}
 
+	/**
+	 * Metodo responsavel por cadastrar um novo usuario no sistema. Criando
+	 * antes uma chave para ele e verificando antes sua existencia no mapa.
+	 * 
+	 * @param nome
+	 *            Nome do usuario.
+	 * @param telefone
+	 *            Telefone do usuario.
+	 * @param email
+	 *            E-mail do usuario.
+	 */
 	public void cadastrarUsuario(String nome, String telefone, String email) {
 
 		ChaveNomeTelefone chave = new ChaveNomeTelefone(nome, telefone);
@@ -38,6 +49,19 @@ public class ControllerUsuario {
 		this.usuarios.put(chave, usuario);
 	}
 
+	/**
+	 * Metodo responsavel por recuperar uma informacao do usuario escolhido.
+	 * Verificando antes sua existencia no mapa.
+	 *
+	 * @param nome
+	 *            Nome do usuario.
+	 * @param telefone
+	 *            Telefone do usuario.
+	 * @param atributo
+	 *            Atributo escolhido para ser retornado.
+	 * 
+	 * @return Retorna uma string com o atributo escolhido.
+	 */
 	public String getInfoUsuario(String nome, String telefone, String atributo) {
 
 		ChaveNomeTelefone chave = new ChaveNomeTelefone(nome, telefone);
@@ -59,12 +83,28 @@ public class ControllerUsuario {
 
 	}
 
+	/**
+	 * Metodo responsavel por verificar a existencia de um usuario no mapa de
+	 * usuarios. Amplamente utilizado nos metodos desse classe.
+	 * 
+	 * @param chave
+	 *            Chave do usuario que sera verificado.
+	 */
 	private void verificaExistenciaChaveMapa(ChaveNomeTelefone chave) {
 
 		if (!this.usuarios.containsKey(chave))
 			Exceptions.usuarioInvalidoException();
 	}
 
+	/**
+	 * Metodo responsavel remover um usuario da lista de usuarios, verificando
+	 * antes sua existencia no mapa de usuarios.
+	 * 
+	 * @param nome
+	 *            Nome do usuario.
+	 * @param telefone
+	 *            Telefone do usuario.
+	 */
 	public void removerUsuario(String nome, String telefone) {
 
 		ChaveNomeTelefone chave = new ChaveNomeTelefone(nome, telefone);
@@ -75,6 +115,20 @@ public class ControllerUsuario {
 
 	}
 
+	/**
+	 * Metodo responsavel por atualizar uma informacao em um usuario.
+	 * Verificando antes sua existencia no mapa de usuarios e a existencia do
+	 * atributo escolhido.
+	 * 
+	 * @param nome
+	 *            Nome do usuario.
+	 * @param telefone
+	 *            Telefone do usuario.
+	 * @param atributo
+	 *            Atributo escolhido para ser retornado.
+	 * @param valor
+	 *            Novo valor do atributo escolhido.
+	 */
 	public void atualizarUsuario(String nome, String telefone, String atributo, String valor) {
 
 		ChaveNomeTelefone chave = new ChaveNomeTelefone(nome, telefone);
@@ -103,6 +157,19 @@ public class ControllerUsuario {
 
 	}
 
+	/**
+	 * Metodo responsavel por identificar um usuario no mapa de usuarios e
+	 * retornar o mesmo. Esse objeto eh amplamente utilizado pelo sistema,
+	 * inclusive sendo chave do mapa de itens.
+	 * 
+	 * @param nome
+	 *            Nome do usuario.
+	 * @param telefone
+	 *            Telefone do usuario.
+	 * 
+	 * @return Retorna um objeto do tipo usuario, que encontra-se no mapa de
+	 *         usuarios.
+	 */
 	public Usuario identificaUsuario(String nome, String telefone) {
 
 		ChaveNomeTelefone chave = new ChaveNomeTelefone(nome, telefone);
@@ -112,15 +179,32 @@ public class ControllerUsuario {
 		return usuarios.get(chave);
 	}
 
+	/**
+	 * Metodo responsavel por adicionar um emprestimo realizado ao conjunto de
+	 * emprestimos realizado de um usuario escolhido. Veficando antes a
+	 * existencia do usuario no mapa de usuarios.
+	 * 
+	 * @param nomeDono
+	 *            Nome do usuario dono do item.
+	 * @param telefoneDono
+	 *            Telefone do usuario dono do item.
+	 * @param nomeRequerente
+	 *            Nome do usuario requerente do item.
+	 * @param telefoneRequerente
+	 *            Telefone do usuario requerente do item.
+	 * @param emprestimo
+	 *            Objeto do tipo Emprestimo que será guardado no conjunto de
+	 *            cada usuario relacionado.
+	 */
 	public void adicionarEmprestimoRealizado(String nomeDono, String telefoneDono, String nomeRequerente,
 			String telefoneRequerente, Emprestimo emprestimo) {
 
 		ChaveNomeTelefone chaveDono = new ChaveNomeTelefone(nomeDono, telefoneDono);
 		ChaveNomeTelefone chaveRequerente = new ChaveNomeTelefone(nomeRequerente, telefoneRequerente);
-		
+
 		verificaExistenciaChaveMapa(chaveDono);
 		verificaExistenciaChaveMapa(chaveRequerente);
-		
+
 		identificaUsuario(nomeDono, telefoneDono).registraEmprestimoRealizado(emprestimo);
 		identificaUsuario(nomeRequerente, telefoneRequerente).registraEmprestimoRealizado(emprestimo);
 	}
