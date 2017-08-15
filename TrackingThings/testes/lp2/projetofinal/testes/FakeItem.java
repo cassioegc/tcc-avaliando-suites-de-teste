0bbe95b29d26fba1ed8fa43536b4ff38b9b7d750
marcelovitorino <marcelo.vitorino@ccc.ufcg.edu.br>
2017-08-15 09:48:50 -0300

    FakeItem e FakeItemTeste criadas

---
+++ TrackingThings/testes/lp2/projetofinal/testes/FakeItem.java
@@ -0,0 +1,12 @@
+package lp2.projetofinal.testes;
+
+import lp2.projetofinal.entidades.Item;
+
+public class FakeItem extends Item {
+	
+	public FakeItem(String nome, double preco) {
+		super(nome,preco);
+	}
+	
+	
+}
