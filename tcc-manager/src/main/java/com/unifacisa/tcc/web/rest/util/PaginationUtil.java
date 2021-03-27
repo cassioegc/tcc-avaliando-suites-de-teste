b458ec8399e7f44c47046dec5e21983fecad0a5f
cassioegc <cassio.cordeiro@ccc.ufcg.edu.br>
2021-03-26 21:38:44 -0300

    f2-r5

--- tcc-manager/src/main/java/com/unifacisa/tcc/web/rest/util/PaginationUtil.java
+++ tcc-manager/src/main/java/com/unifacisa/tcc/web/rest/util/PaginationUtil.java
@@ -21,13 +21,6 @@ public final class PaginationUtil {
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-Total-Count", Long.toString(page.getTotalElements()));
         String link = "";
-        if ((page.getNumber() + 1) < page.getTotalPages()) {
-            link = "<" + generateUri(baseUrl, page.getNumber() + 1, page.getSize()) + ">; rel=\"next\",";
-        }
-        // prev link
-        if ((page.getNumber()) > 0) {
-            link += "<" + generateUri(baseUrl, page.getNumber() - 1, page.getSize()) + ">; rel=\"prev\",";
-        }
         // last and first link
         int lastPage = 0;
         if (page.getTotalPages() > 0) {
@@ -37,6 +30,17 @@ public final class PaginationUtil {
         link += "<" + generateUri(baseUrl, 0, page.getSize()) + ">; rel=\"first\"";
         headers.add(HttpHeaders.LINK, link);
         return headers;
+    }
+
+    private static String getString(Page page, String baseUrl, String link) {
+        if ((page.getNumber() + 1) < page.getTotalPages()) {
+            link = "<" + generateUri(baseUrl, page.getNumber() + 1, page.getSize()) + ">; rel=\"next\",";
+        }
+        // prev link
+        if ((page.getNumber()) > 0) {
+            link += "<" + generateUri(baseUrl, page.getNumber() - 1, page.getSize()) + ">; rel=\"prev\",";
+        }
+        return link;
     }
 
     private static String generateUri(String baseUrl, int page, int size) {
